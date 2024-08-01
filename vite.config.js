beb302f2bc996cd0976e948ad247e0cf9c988148
Jacob Paris <jacob@jacobparis.com>
2024-08-01 16:52:12 +0800

    fix: switch to tailwind vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { vitePlugin as remix } from "@remix-run/dev"
+import tailwind from "@tailwindcss/vite"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [remix()],
+  plugins: [
+    remix(),
+    tailwind({
+      content: ["./app/**/*.{js,jsx,ts,tsx}"],
+      theme: {
+        extend: {},
+      },
+      plugins: [],
+    }),
+  ],
 })
