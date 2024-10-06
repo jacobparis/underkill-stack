acdbc53e2a9c27e8a20bba18fb2174fd2f19fcc6
Jacob Paris <jacob@jacobparis.com>
2024-10-06 05:07:00 -0400

    fix: tailwind config not needed

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,6 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [
     reactRouter(),
-    tailwind({
-      content: ["./app/**/*.{js,jsx,ts,tsx}"],
-      theme: {
-        extend: {},
-      },
-      plugins: [],
-    }),
+    tailwind(),
   ],
 });
